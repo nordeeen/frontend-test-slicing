9b2b4871629a548e8e89ed93468a06bfd4bfc9ec
nordeeen <ahmadnrdnsah@gmail.com>
2023-01-11 12:24:21 +0700

    add swiper image in MainSwiper comp

--- src/components/MainSwiper.jsx
+++ src/components/MainSwiper.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import smallContent from '../assets/small-content.png';
-import fakeImg from '../assets/fake-img.png';
-import OvalBlue from '../assets/oval-blue.png';
+import Testi1 from '../assets/testi1.png';
+import Testi2 from '../assets/testi2.png';
+import Testi3 from '../assets/testi3.png';
+import Testi4 from '../assets/testi4.png';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import 'swiper/css';
 
 const MainSwiper = () => {
+  const swiper = useSwiper();
   return (
     <div className="w-full h-auto bg-white px-6 flex justify-between items-center flex-col py-16">
       <img
@@ -23,22 +29,79 @@ const MainSwiper = () => {
           kualitas sampah dan limbah secara real-time
         </p>
       </div>
-      <div className="w-[60%] flex justify-start items-center">
-        <img
-          src={OvalBlue}
-          alt="icon-oval"
-          loading="eager"
-          className="w-[100px] h-[100px] object-contain"
-        />
+      <div className="py-6">
+        <button
+          className="bg-green-500 rounded-full w-4 h-4 mr-4"
+          onClick={() => swiper.slideNext()}
+        ></button>
+        <button
+          className="bg-green-500 rounded-full w-4 h-4 mr-4"
+          onClick={() => swiper.slideNext()}
+        ></button>
+        <button
+          className="bg-green-500 rounded-full w-4 h-4 mr-4"
+          onClick={() => swiper.slideNext()}
+        ></button>
+        <button
+          className="bg-green-500 rounded-full w-4 h-4 mr-4"
+          onClick={() => swiper.slideNext()}
+        ></button>
       </div>
-      <img
-        src={fakeImg}
-        alt="img"
-        loading="eager"
-        className="w-[100%] h-auto object-contain py-8"
-      />
+      <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        centeredSlidesBounds={true}
+        slidesPerView={1}
+        className="flex items-center justify-center"
+        style={{ width: '50%' }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img
+            src={Testi1}
+            alt="icon-oval"
+            loading="eager"
+            className="object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={Testi2}
+            alt="icon-oval"
+            loading="eager"
+            className=" object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={Testi3}
+            alt="icon-oval"
+            loading="eager"
+            className=" object-contain"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          {' '}
+          <img
+            src={Testi4}
+            alt="icon-oval"
+            loading="eager"
+            className=" object-contain"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
-}
+};
 
-export default MainSwiper
+export default MainSwiper;
